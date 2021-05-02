8050fb444aa9cea3c933edc25a6dadddb697f036
Pierre Petillot <petill_p@etna-alternance.net>
2021-05-02 18:15:27 +0200

    3 feature : error messages and validation implemented

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,3 +1,54 @@
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+const modalClose = document.querySelector(".close");
+const formulaire = document.getElementById('formulaire');
+const firstInput = document.getElementById('first');
+const nameInput = document.getElementById('last');
+const mailInput = document.getElementById('email');
+const dateInput = document.getElementById('birthdate');
+const quantityInput = document.getElementById('quantity');
+const radioInput = document.querySelector('radio');
+
+
+// modal events
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalClose.addEventListener('click', hideModal);
+
+// error form
+firstInput.addEventListener('input', function() {
+  hideError(firstInput);
+  if (!isFirstNameValid())
+  {
+    showError(firstInput);
+  }
+});
+
+nameInput.addEventListener('input', function() {
+  hideError(nameInput);
+  if (!isNameValid())
+  {
+    showError(nameInput);
+  }
+});
+
+mailInput.addEventListener('input', function() {
+  hideError(mailInput);
+  if (!emailIsValid(mailInput.value))
+  {
+    showError(mailInput);
+  }
+});
+
+quantityInput.addEventListener('input', function(){
+  hideError(quantityInput);
+  if (isQuantityValid)
+  {
+    showError(quantityInput);
+  }
+});
+
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -7,48 +58,98 @@ function editNav() {
   }
 }
 
-// DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-const modalClose = document.querySelector(".close");
-
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-// launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-// close modal form
+function hideModal() {
+  modalbg.style.display = "none";
+}
 
-modalClose.addEventListener('click', function() {
-    modalbg.style.display = "none";
-});
+function showError(field) {
+  let parent = field.closest('div');
+  parent.setAttribute('data-error-visible', true);
+}
 
-// formulaire si erreur
+function hideError(field) {
+  let parent = field.closest('div');
+  parent.setAttribute('data-error-visible', false);
+}
 
+function hasNumber(myString) {
+  return /\d/.test(myString);
+}
 
+function isFirstNameValid() {
 
-// conditions validité form
+  if (firstInput.value.length <= 2 || firstInput.value.length > 40)
+  {
+    return false;
+  }
+  if (hasNumber(firstInput.value))
+  {
+    return false;
+  }
+  else
+  {
+    return true;
+  }
+}
 
-const formulaire = document.getElementById('formulaire');
-const firstInput = document.getElementById('first');
-const nameInput = document.getElementById('last');
-const mailInput = document.getElementById('email');
-const dateInput = document.getElementById('birthdate');
-const quantityInput = document.getElementById('quantity');
-const radio1 = document.getElementById('location1');
+function isNameValid()
+{
 
-firstInput.addEventListener("input", function(e) {
-  let value = e.target.value;
-  if (value.length >= 2) {
-    isValid = true;
+  if (nameInput.value.length <= 2 || nameInput.value.length > 40)
+  {
+    return false;
+  }
+  if (hasNumber(nameInput.value))
+  {
+    return false;
+  }
+  else
+  {
+    return true;
+  }
+}
+
+function emailIsValid (email) {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
+
+function isQuantityValid() {
+  if (quantityInput.value.length == 0)
+  {
+    return false;
+  }
+  else
+  {
+    return true;
+  }
+}
+
+function atLeastOneRadio() {
+  return ($('input[type=radio]:checked').size() > 0);
+}
+
+function isFormValid() {
+  let verif = false;
+  if (isFirstNameValid && isNameValid && emailIsValid)
+  {
+    verif = true;
   }
   else {
-    isValid = false;
+    verif = false;
   }
-});
+  return verif;
+}
 
-
+function displayBtn() {
+  let submitBtn = document.querySelector('btn-submit');
+  if (!isFormValid)
+  {
+    submitBtn.setAttribute('disabled', true);
+    submitBtn.style.opacity = 0.5;
+    submitBtn.style.cursor = 'not-allowed';
+  }
+}
